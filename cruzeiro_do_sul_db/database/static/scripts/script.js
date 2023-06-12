2053c80400d4d4630c4981379f036fb62e15b0ab
James Moraes de Almeida <james.almeida@i-tmp14-l.abtlus.org.br>
2023-06-12 15:05:15 -0300

    Reuploading previous commit

---
+++ cruzeiro_do_sul_db/database/static/scripts/script.js
@@ -0,0 +1,30 @@
+window.onload = SearchMethodCheck();
+
+function SearchMethodCheck() {
+  x = document.getElementById("SelectSearch");
+  if (x) {
+    if (x.value == "Comparation") {
+      document.getElementById("ComparationEnergyLegend").style.display = "block";
+      document.getElementById("ComparationEnergyFile").style.display = "block";
+      document.getElementById("ComparationMuLegend").style.display = "block";
+      document.getElementById("ComparationMuFile").style.display = "block";
+    }
+    else {
+      document.getElementById("ComparationEnergyLegend").style.display = "none";
+      document.getElementById("ComparationEnergyFile").style.display = "none";
+      document.getElementById("ComparationMuLegend").style.display = "none";
+      document.getElementById("ComparationMuFile").style.display = "none";
+    }
+  }
+}
+
+function AddElement(string) {
+  x = document.getElementById("absorbing_element")
+  y = document.getElementById("composition");
+  if (x) {
+    x.value = string;
+  }
+  if (y) {
+    y.value += string + ' ';
+  }
+}
